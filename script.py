fcdc95c07ea7887407063e8ec0da039eab8e603b
Axlfc <axelfernandezcurros@gmail.com>
2021-12-26 13:44:23 +0100

    UPDATED: script.py

--- script.py
+++ script.py
@@ -3,41 +3,35 @@ import colorama
 from colorama import Fore, Style
 import blockcypher
 from moneywagon import AddressBalance
-
 from forex_python.bitcoin import BtcConverter
 
 def balance(addr):
     try:
         total = float(blockcypher.get_total_balance(addr))
-        print("Balance of address wallet " + addr + "is:\t" + str(total))
+        #print("Balance of address wallet " + addr + "is:\t" + str(total))
         return total
     except:
         total = float(AddressBalance().action("btc", addr))
-        print("Balance (€) of address wallet " + addr + " is:\t" + str(total))
+        #print("Balance (€) of address wallet " + addr + " is:\t" + str(total))
         return total
 
 
 def main(eur):
-    #btcaddr = input("Enter an Address:\t")
-
-    #print(str(balance(btcaddr) * eur) + "€")
-
-
-    fakeBTCamount = 0.0097779
-    inve_dirt = fakeBTCamount * eur
-    #print(str(round(inve_dirt, 2)) + "€")
-    #inve_dirt = balance(btcaddr) * eur
     inve = 300
+
+    btcaddr = input("Enter an Address:\t")
+
+    inve_dirt = balance(btcaddr) * eur
+
+    #fakeBTCamount = 0.0097779
+    #inve_dirt = fakeBTCamount * eur
+
     rel = inve_dirt / inve
     if rel > 1:
-        #print("You're winning")
-        winrate = (rel / 100) * 100
-        print(Fore.GREEN + "WIN: " + Fore.RESET + str(winrate) + "%")
+        print(Fore.GREEN + "WIN: " + Fore.RESET + str(rel) + "%")
     else:
-        #print("Sorry to lose")
-        winrate = (rel / 100) * 100
-        print(Fore.RED + "LOSE: " + Fore.RESET + str(winrate) + "%")
-        #return winrate
+        print(Fore.RED + "LOSE: " + Fore.RESET + str(rel) + "%")
+
     balance_dirty = inve_dirt - inve
     if balance_dirty > 0:
         balance_dirty = "+" + str(round(balance_dirty, 2))
@@ -50,10 +44,6 @@ def main(eur):
 
 
 if __name__ == '__main__':
-    #inve = int(input("Enter invest:\t"))
-
-    #amou = int(input("Calculate actual wallet amount:\t"))
-
     b = BtcConverter()  # force_decimal=True to get Decimal rates
     b.get_latest_price('EUR')
     #print("1 BTC = " + str(b.get_latest_price('EUR')) + "€")

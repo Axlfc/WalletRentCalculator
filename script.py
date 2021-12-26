01602015a2e2f8d3623ff87aa7924a85b138e5ea
Axlfc <axelfernandezcurros@gmail.com>
2021-12-26 13:22:23 +0100

    UPDATED: script.py

--- script.py
+++ script.py
@@ -9,11 +9,11 @@ from forex_python.bitcoin import BtcConverter
 def balance(addr):
     try:
         total = float(blockcypher.get_total_balance(addr))
-        #print("Balance of address wallet " + addr + "is:\t" + str(total))
+        print("Balance of address wallet " + addr + "is:\t" + str(total))
         return total
     except:
         total = float(AddressBalance().action("btc", addr))
-        #print("Balance (€) of address wallet " + addr + " is:\t" + str(total))
+        print("Balance (€) of address wallet " + addr + " is:\t" + str(total))
         return total
 
 
@@ -25,17 +25,28 @@ def main(eur):
 
     fakeBTCamount = 0.0097779
     inve_dirt = fakeBTCamount * eur
-    print(str(round(inve_dirt, 2)) + "€")
+    #print(str(round(inve_dirt, 2)) + "€")
+    #inve_dirt = balance(btcaddr) * eur
     inve = 300
     rel = inve_dirt / inve
     if rel > 1:
         #print("You're winning")
         winrate = (rel / 100) * 100
-        print(Fore.GREEN + "WIN: " + str(winrate) + "%")
+        print(Fore.GREEN + "WIN: " + Fore.RESET + str(winrate) + "%")
     else:
         #print("Sorry to lose")
         winrate = (rel / 100) * 100
-        print(Fore.RED + "LOSE: " + str(winrate) + "%")
+        print(Fore.RED + "LOSE: " + Fore.RESET + str(winrate) + "%")
+        #return winrate
+    balance_dirty = inve_dirt - inve
+    if balance_dirty > 0:
+        balance_dirty = "+" + str(round(balance_dirty, 2))
+    elif balance_dirty == 0:
+        balance_dirty = str(balance_dirty)
+    else:
+        balance_dirty = "-" + str(round(balance_dirty, 2))
+
+    print("Balance is:\t" + Fore.GREEN + balance_dirty + "€")
 
 
 if __name__ == '__main__':
